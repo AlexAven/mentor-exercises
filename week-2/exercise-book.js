d7b370f29628e0caf9e98ba7e2b00db917621bf2
Alex Kazakov <gloom31@gmail.com>
2024-02-11 20:50:49 +0300

    new commit

--- week-2/exercise-book.js
+++ week-2/exercise-book.js
@@ -22,18 +22,19 @@ function EvenCheck(num) {
   let result = num % 2 === 0 ? 'Number is even' : 'Number is odd';
   return result;
 };
- // 5. Даны два слова. Проверьте, что первые буквы этих слов совпадают.
- function firstLettersCheck(str1, str2) {
+// 5. Даны два слова. Проверьте, что первые буквы этих слов совпадают.
+function firstLettersCheck(str1, str2) {
   let result = str1[0].toLowerCase() === str2[0].toLowerCase() ? 'Characters match' : 'Charecters don\'t match';
   return result;
- };
+};
 
 // 6. Дано слово. Получите его последнюю букву. 
 //Если слово заканчивается на мягкий знак, то получите предпоследнюю букву.
 function letterReturn(str) {
-  let result = str.toLowerCase().endsWith('ь') ? str.charAt(str.length -2) : str.charAt(str.length -1);
+  let result = str.toLowerCase().endsWith('ь') ? str.charAt(str.length - 2) : str.charAt(str.length - 1);
   return result;
 };
+
 
 // 1.2
 // 1. Дано число. Выведите в консоль первую цифру этого числа.
@@ -45,7 +46,7 @@ function firstDigitReturn(num) {
 // 2. Дано число. Выведите в консоль последнюю цифру этого числа.
 function lastDigitReturn(num) {
   num = num.toString();
-  console.log(Number(num[num.length -1]));
+  console.log(Number(num[num.length - 1]));
 };
 
 // 3. Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
@@ -67,11 +68,169 @@ function numbersCheck(num1, num2) {
 };
 
 
+// 1.3
+// 1. Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.
+function returnStr(str) {
+  console.log(str.length > 1 ? str[str.length - 2] : 'string length is too short!');
+};
+
+// 2. Даны два целых числа. Проверьте, что первое число без остатка делится на второе.
+function divideWithoutRemainder(num1, num2) {
+  return num1 % num2 === 0;
+};
+
+
+// 1.4
+// 1. Выведите в консоль все целые числа от 1 до 100.
+(() => {
+  let counter = 1;
+
+  while (counter <= 100) {
+    console.log(counter);
+    counter++
+  };
+})();
+
+// 2. Выведите в консоль все целые числа от -100 до 0.
+(() => {
+  let counter = -100;
+
+  while (counter !== 1) {
+    console.log(counter);
+    counter++
+  };
+})();
+
+// 4. Выведите в консоль все четные числа из промежутка от 1 до 100.
+(() => {
+  let counter = 1;
+
+  while (counter <= 100) {
+    counter % 2 === 0 ? console.log(counter) : null;
+    counter++;
+  }
+})();
+
+// 5. Выведите в консоль все числа кратные трем в промежутке от 1 до 100.
+(() => {
+  let counter = 1;
+
+  while (counter <= 100) {
+    counter % 3 === 0 ? console.log(counter) : null;
+    counter++;
+  }
+})();
+
+
+// 1.5
+// 1. Найдите сумму всех целых чисел от 1 до 100.
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+};
+console.log(sum);
+
+// 2. Найдите сумму всех целых четных чисел в промежутке от 1 до 100.
+let sum = 0;
+for (let i = 2; i <= 100; i +=2) {
+  sum += i;
+};
+console.log(sum);
+
+// 3. Найдите сумму всех целых нечетных чисел в промежутке от 1 до 100.
+let sum = 0;
+for (let i = 1; i <= 100; i +=2) {
+  sum += i;
+};
+console.log(sum);
+
+// 4. Даны два целых числа. Найдите остаток от деления первого числа на второе.
+function returnRemainder(num1, num2) {
+  return num1 % num2;
+};
+
+// 5. Дана некоторая строка. Переберите и выведите в консоль по очереди все символы с конца строки.
+function reverseReturn(str) {
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i]);
+  };
+};
+
+
+// 1.6
+// 1. Дан массив с числами. Найдите сумму квадратов элементов этого массива.
+function sumOfSquare(arr) {
+  return arr.reduce((acc, elem) => acc + Math.pow(elem, 2), 0);
+};
+
+// 2. Дан массив с числами. Найдите сумму квадратных корней элементов этого массива.
+function sumOfSquare(arr) {
+  return arr.reduce((acc, elem) => acc + Math.sqrt(elem), 0);
+};
+
+// 3. Дан массив с числами. Найдите сумму положительных элементов этого массива.
+function sumPositiveElements(arr) {
+  return arr.reduce((acc, elem) => {
+    if (elem >= 0) {
+      return acc + elem
+    } else {
+      return acc;
+    };
+  }, 0)
+};
+
+// 4. Дан массив с числами. Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.
+function sumOfNumbers(arr) {
+  return arr.reduce((acc, elem) => {
+    if (elem > 0 && elem < 10) {
+      return acc + elem;
+    } else {
+      return acc;
+    }
+  }, 0);
+};
+
+
+// 1.7
+// 1. Дана строка:
+//   'abcde'
+//    Получите массив букв этой строки.
+
+function makeAnArray(str) {
+  return str.split('');
+};
+
+// 2.  Дано некоторое число:
+//     12345
+//     Получите массив цифр этого числа.
+function makeAnArrayOfDigits(num) {
+  num = num.toString().split('');
+  return num.map(elem => Number(elem));
+  };
+
+   // or 
+
+function makeAnArrayOfDigits(num) {
+  return Array.from(String(num), Number);
+  };
+
+// 3. Дано некоторое число:
+//    12345
+//    Переверните его:
+//    54321
+
+
+
+
+
+
+
+
 // 2.1
 
 // 1. Дана некоторая строка. Найдите позицию первого нуля в строке.
 function findFirstZero() {
-
+//
 }
 
 
