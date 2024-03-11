e2d894715fafeda2b6b7f7eb6b1e225d0b0f7ef9
Alex Kazakov <gloom31@gmail.com>
2024-03-11 20:37:55 +0300

    new-exercices

---
+++ week-3/numbers-range-creator.js
@@ -0,0 +1,25 @@
+// Написать функцию, которая сжимает целочисленный массив следующим образом. 
+// Подряд идущие несколько чисел объединяются в диапазон x-y (например, 1,2,3,4,5,6,7,8 => 1-8), 
+// вся последовательность сжимается в строку, а ее элементы разделяются запятыми.
+
+function digitRangeCreator(array) {
+  let sequenceOfNumbers = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (Math.abs(array[i] - array[i + 1]) === 1) {
+      sequenceOfNumbers.push(array[i]);
+    } else {
+      if (sequenceOfNumbers.length > 0) {
+        sequenceOfNumbers.push(array[i]);
+        break
+      }
+    }
+  };
+
+  return sequenceOfNumbers.length > 0
+    ?
+    `${sequenceOfNumbers.join(',')} => ${String(sequenceOfNumbers.at(0))}-${String(sequenceOfNumbers.at(-1))}`
+    :
+    'There is no sequence!';
+};
